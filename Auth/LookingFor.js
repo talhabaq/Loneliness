f42d881469d925e2c53b1114a51e1a45e9c426a1
talhabaq <talhabaqir864@gmail.com>
2024-10-10 15:03:45 +0500

    Loneliness

---
+++ Auth/LookingFor.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LookingFor = () => {
+  return (
+    <View>
+      <Text>LookingFor</Text>
+    </View>
+  )
+}
+
+export default LookingFor
